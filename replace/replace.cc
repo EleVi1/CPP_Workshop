7c3ae93e5e8d7a56f59f25ecd6132cdf2d901991
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-26 17:48:33 +0100

    second

--- replace/replace.cc
+++ replace/replace.cc
@@ -4,32 +4,31 @@ void replace(const std::string& input_filename,
              const std::string& output_filename, const std::string& src_token,
              const std::string& dst_token)
 {
-    std::ifstream file_in;
-    file_in.open(input_filename);
-    if (!file_in.is_open())
+    std::ifstream input_file;
+    input_file.open(input_filename);
+    if (!input_file.is_open())
     {
         std::cerr << "Cannot open input file\n";
         return;
     }
 
-    std::ofstream file_out;
-    file_out.open(output_filename);
-    if (!file_out.is_open())
+    std::ofstream output_file;
+    output_file.open(output_filename);
+    if (!output_file.is_open())
     {
         std::cerr << "Cannot write output file\n";
         return;
     }
 
     std::string line;
-    size_t index;
-    while (std::getline(file_in, line))
+    while (std::getline(input_file, line))
     {
-        index = line.find(src_token);
+        size_t index = line.find(src_token);
         while (index != std::string::npos)
         {
             line.replace(index, src_token.length(), dst_token);
             index = line.find(src_token, index + dst_token.length());
         }
-        file_out << line << '\n';
+        output_file << line << '\n';
     }
 }
