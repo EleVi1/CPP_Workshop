417caf1efd559cfcf1e6327af9ff33e1d20b38cd
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-28 18:44:03 +0100

    second

--- for_each/for_each.hxx
+++ for_each/for_each.hxx
@@ -4,7 +4,7 @@
 template <class T, class U, class Function>
 void my_foreach(T beg, U end, Function f)
 {
-    for (; beg < end; beg++)
+    for (; beg != end; beg++)
     {
         f(*beg);
     }
