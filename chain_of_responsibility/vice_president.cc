ff47aeb5958fed79486f42547293d560215d0b8b
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-29 18:30:32 +0100

    first

---
+++ chain_of_responsibility/vice_president.cc
@@ -0,0 +1,15 @@
+#include "vice_president.hh"
+
+#include <iostream>
+
+void VicePresident::handle_request(int level)
+{
+    if (level <= 6)
+    {
+        std::cout << "VicePresident handles\n";
+    }
+    else
+    {
+        this->forward_request(level);
+    }
+}
