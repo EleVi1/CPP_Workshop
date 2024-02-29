a8e0e506aa6d9a0e14d2e30239462c29ed80652d
Elena Villepreux <elena.villepreux@epita.fr>
2024-02-29 22:15:28 +0100

    first

---
+++ implements/implements.hh
@@ -0,0 +1,45 @@
+
+template <typename U, typename V = U>
+concept impl_basic_op = requires(U u, V v)
+{
+    { u + v };
+    { v + u };
+    { +u };
+    { +v };
+    { -u };
+    { -v };
+    { u - v };
+    { v - u };
+    { u * v };
+    { v * u };
+    { v / u };
+    { u / v };
+};
+
+template <typename U, typename V = U>
+concept impl_modulo = requires(U u, V v)
+{
+    { v % u };
+    { u % v };
+};
+
+template <typename U, typename V = U>
+concept impl_bitwise_op = requires(U u, V v)
+{
+    { ~u };
+    { ~v };
+    { v | u };
+    { u | v };
+    { v ^ u };
+    { u ^ v };
+    { v << u };
+    { u << v };
+    { v >> u };
+    { u >> v };
+    { v & u };
+    { u & v };
+};
+
+template <typename U, typename V = U>
+concept impl_arith_op =
+    impl_basic_op<U, V> && impl_bitwise_op<U, V> && impl_modulo<U, V>;
