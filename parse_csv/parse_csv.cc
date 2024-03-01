a7bd128675180ca7ed6dd0de22a72a82044837a2
Elena Villepreux <elena.villepreux@epita.fr>
2024-03-01 13:52:25 +0100

    second

--- parse_csv/parse_csv.cc
+++ parse_csv/parse_csv.cc
@@ -11,58 +11,38 @@ std::vector<std::vector<std::string>> parse_csv(const std::string& file_name)
         throw std::ios_base::failure("Error opening file");
     }
     std::string line;
-    std::getline(file, line);
     std::string word;
     size_t elements = 0;
-    size_t pos;
-    // First line : store and count elements on column
-    std::vector<std::string> input;
-    while ((pos = line.find(",")) != std::string::npos)
-    {
-        word = line.substr(0, pos);
-        line.erase(0, pos + 1);
-        // std::cout << "Word: " << word << '\n';
-        elements++;
-        input.push_back(word);
-    }
-    word = line.substr(0, pos);
-    line.erase(0, pos + 1);
-    // std::cout << "Word: " << word << '\n';
-    elements++;
-    input.push_back(word);
-    result.push_back(input);
-
-    size_t count = 0;
-    size_t line_nb = 2;
-    std::stringstream errMsg;
+    size_t line_nb = 1;
     while (std::getline(file, line))
     {
+        std::stringstream lineStream(line);
         std::vector<std::string> input;
-        while (count < elements - 1
-               && (pos = line.find(",")) != std::string::npos)
+        if (line_nb == 1)
         {
-            word = line.substr(0, pos);
-            line.erase(0, pos + 1);
-            // std::cout << "Word: " << word << '\n';
-            count++;
-            input.push_back(word);
+            while (std::getline(lineStream, word, ','))
+            {
+                elements++;
+                input.push_back(word);
+            }
         }
-        word = line.substr(0, pos);
-        line.erase(0, pos + 1);
-        // std::cout << "Word: " << word << '\n';
-        count++;
-        input.push_back(word);
-
-        if (count != elements
-            || (pos = line.find(","))
-                != std::string::npos) // too few or more than the precedent
-                                      // column
+        else
         {
-            errMsg << "Non consistent number of columns at line " << line_nb;
-            throw std::ios_base::failure(errMsg.str());
+            size_t count = 0;
+            while (count < elements && std::getline(lineStream, word, ','))
+            {
+                count++;
+                input.push_back(word);
+            }
+            if (count != elements || std::getline(lineStream, word, ','))
+            {
+                std::stringstream errMsg;
+                errMsg << "Non consistent number of columns at line "
+                       << line_nb;
+                throw std::ios_base::failure(errMsg.str());
+            }
         }
         result.push_back(input);
-        count = 0;
         line_nb++;
     }
     return result;
